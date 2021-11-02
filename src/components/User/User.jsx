abf7f7b54b75db73df8c27b11b157780f6a59780
samdev <samuelberrus@gmail.com>
2021-11-02 20:33:59 +0100

    Trabajando en la lógica de las transacciones y en login

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -16,74 +16,85 @@ const User = () => {
 	const [user, setUser] = useState(null);
 	const [userTransactions, setUserTransactions] = useState(null);
 	useEffect(() => {
-		axios
-			.get("https://zbank.samdev.es/v1/users?userID=4f62")
-			.then(({ data }) => setUser(data));
-		axios
-			.get("https://zbank.samdev.es/v1/transactions?accountID=4f62")
-			.then(({ data }) => {
-				setUserTransactions(data);
-			});
-	}, []);
+		let _userID = "4f62";
+		const callData = async () => {
+			await axios
+				.get(`https://zbank.samdev.es/v1/users?userID=${_userID}`)
+				.then(({ data }) => setUser(data));
+			await axios
+				.get(
+					`https://zbank.samdev.es/v1/transactions?accountID=${_userID}`
+				)
+				.then(({ data }) => {
+					const last6Transactions = data.slice(-6).reverse();
+					setUserTransactions(last6Transactions);
+				});
+		};
+		callData();
+	}, [setUser, setUserTransactions]);
 
 	return tokenExists ? (
 		<div className="container p-0">
 			<Header2 user={user} />
 			<hr />
 
-			<div id="canva-grafico">
-				<BarChart userTransactions={userTransactions} />
-			</div>
+			{userTransactions && (
+				<div id="canva-grafico">
+					<BarChart userTransactions={userTransactions} user={user} />
+				</div>
+			)}
 			<hr />
-			<div className="w-100">
-				<h2>Últimos registros</h2>
-				<table className="table text-light">
-					<thead>
-						<tr>
-							<th scope="col">Concepto</th>
-							<th scope="col">Monto</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<th scope="row">Concepto 1</th>
-							<td className="text-success fw-bold">+250</td>
-							<td>
-								<Link
-									to="/transaction"
-									className="btn btn-outline-primary"
-								>
-									Detalles[]
-								</Link>
-							</td>
-						</tr>
-						<tr>
-							<th scope="row">Concepto 2</th>
-							<td className="text-danger fw-bold">-500</td>
-							<td>
-								<Link
-									to="/transaction"
-									className="btn btn-outline-primary"
-								>
-									Detalles[]
-								</Link>
-							</td>
-						</tr>
-						<tr>
-							<th scope="row">concepto 3</th>
-							<td className="text-success fw-bold">+1500</td>
-							<td>
-								<Link
-									to="/transaction"
-									className="btn btn-outline-primary"
-								>
-									Detalles[]
-								</Link>
-							</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
+			{userTransactions && (
+				<div className="w-100">
+					<h2>Últimos registros</h2>
+					<table className="table text-light">
+						<thead>
+							<tr>
+								<th scope="col">Concepto</th>
+								<th scope="col">Monto</th>
+							</tr>
+						</thead>
+						<tbody>
+							{userTransactions.map((transaction) => (
+								<tr key={transaction.transactionID}>
+									<th scope="row ">
+										{user.userID === transaction.sender ? (
+											<span className="text-danger">
+												Enviado
+											</span>
+										) : (
+											<span className="text-success">
+												Recibido
+											</span>
+										)}
+									</th>
+									{user.userID === transaction.sender ? (
+										<td className="fw-bold text-danger">
+											{transaction.ammount}
+										</td>
+									) : (
+										<td className="fw-bold text-success">
+											{transaction.ammount}
+										</td>
+									)}
+
+									<td>
+										<Link
+											to="/transaction"
+											className="btn btn-outline-primary"
+										>
+											[...]
+										</Link>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+					<Link to="/" className="link-primary text-center col-12 d-block my-2">
+						Ver todas las transacciones
+					</Link>
+				</div>
+			)}
 		</div>
 	) : (
 		<Redirect to="/login" />
--- src/components/User/components/BarChart.jsx
+++ src/components/User/components/BarChart.jsx
@@ -1,49 +1,32 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
 
-const BarChart = () => {
-	//todo: llamar las últimas 5 transacciones y de los datos extraer los siguientes parametros:
-	//todo: 	Nombre Contraparte: (receptor en el caso de enviar dinero, y emisor de lo contrario).
-	//todo:		saldo a favor: BOOL (para pintar en la gráfica las transferencias de manera más ordenada).
-
-	const last5Trans = [
-		"Tran 1",
-		"Tran 2",
-		"Tran 3",
-		"Tran 4",
-		"Tran 5",
-		"Tran 6",
-	];
-	const colorStatusBG = [
-		"#1d4f2b",
-		"#1d4f2b",
-		"#6e2921",
-		"#6e2921",
-		"#1d4f2b",
-		"#6e2921",
-	];
-	const colorStatusBorder = [
-		"#00ff47",
-		"#00ff47",
-		"#ff1900",
-		"#ff1900",
-		"#00ff47",
-		"#ff1900",
-	];
+const BarChart = ({ userTransactions, user }) => {
+	const data = userTransactions.map((transaction) => {
+		return transaction.ammount;
+	});
+	const labels = userTransactions.map((transaction) => {
+		return user.userID === transaction.sender ? "enviado" : "recibido";
+	});
+	const backgroundColor = userTransactions.map((transaction) => {
+		return user.userID === transaction.sender ? "#6e2921" : "#1d4f2b";
+	});
+	const borderColor = userTransactions.map((transaction) => {
+		return user.userID === transaction.sender ? "#ff1900" : "#00ff47";
+	});
 
 	return (
 		<div>
 			<Bar
 				id="barchart"
 				data={{
-					labels: last5Trans,
+					labels,
 					datasets: [
 						{
 							label: "Montos",
-							data: [120, 300, -500, -500, 2000, -300],
-							//Fondos: verde saldo a favor (#1d4f2b), rojo saldo en contra
-							backgroundColor: colorStatusBG,
-							borderColor: colorStatusBorder,
+							data,
+							backgroundColor,
+							borderColor,
 							borderWidth: 1,
 						},
 					],
@@ -53,8 +36,7 @@ const BarChart = () => {
 
 					plugins: {
 						title: {
-							display: true,
-							text: "Hola",
+							display: false,
 						},
 						legend: {
 							display: false,
